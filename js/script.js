e2dccdbdadd1a2377e5432f78440d50c5fcc2523
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-07-25 19:22:30 +0300

    created third panel (hardcode)

--- js/script.js
+++ js/script.js
@@ -5,13 +5,10 @@ jQuery(document).ready(function() {
     jQuery("#second").css({ width: 0, opacity: 0 });
 
     var duration = 500;
-
     setTimeout(function() {
 
         var w = jQuery("#first").width();
-
         jQuery("#first").animate({width: 0, opacity: 0}, duration);
-
 
         jQuery("#second").css({ opacity: 1 });
         jQuery("#second").animate({width: w}, duration);
