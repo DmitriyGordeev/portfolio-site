64a631cd843b1b95cf4f7bb8b892f23203510f47
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-07-25 22:33:00 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -44,7 +44,6 @@ jQuery(document).ready(function() {
 
             active++;
         }
-
     });
 
 
@@ -65,59 +64,9 @@ jQuery(document).ready(function() {
             stripes.eq(active - 1).css({ opacity: 1 });
             stripes.eq(active - 1).animate({ width: stripeWidth }, page_change_duration);
 
-
             active--;
         }
 
     });
-
-
-
-    // // text-description:
-    // jQuery("#second").css({ width: 0, opacity: 0 });
-    // jQuery("#third").css({ width: 0, opacity: 0 });
-
-
-    //
-    // var duration = 500;
-    // setTimeout(function() {
-    //
-    //     // text-description:
-    //     var w = jQuery("#first").width();
-    //     jQuery("#first").animate({width: 0, opacity: 0}, duration);
-    //
-    //     jQuery("#second").css({ opacity: 1 });
-    //     jQuery("#second").animate({width: w}, duration);
-    //
-    //
-    //     // gallery-stripe:
-    //     var sw = jQuery("#stripe-1").width();
-    //     jQuery("#stripe-1 img").animate({opacity: 0}, duration * 0.4);
-    //     jQuery("#stripe-1").animate({ width: 0}, duration);
-    //
-    //     jQuery("#stripe-2").css({ opacity: 1 });
-    //     jQuery("#stripe-2").animate({ width: sw }, duration);
-    //
-    //
-    // }, 2000);
-    //
-    // setTimeout(function() {
-    //
-    //     var w = jQuery("#second").width();
-    //     jQuery("#second").animate({width: 0, opacity: 0}, duration);
-    //
-    //     jQuery("#third").css({ opacity: 1 });
-    //     jQuery("#third").animate({width: w}, duration);
-    //
-    //
-    //     // gallery-stripe:
-    //     var sw = jQuery("#stripe-2").width();
-    //     jQuery("#stripe-2 img").animate({opacity: 0}, duration * 0.4);
-    //     jQuery("#stripe-2").animate({ width: 0}, duration);
-    //
-    //     jQuery("#stripe-3").css({ opacity: 1 });
-    //     jQuery("#stripe-3").animate({ width: sw }, duration);
-    //
-    // }, 4000);
 
 });
