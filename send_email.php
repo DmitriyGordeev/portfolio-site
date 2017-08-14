97670195706c96ab6de91e29cea7f53aeff0ac11
dimas <user@user.com>
2017-08-14 17:07:39 +0300

    email fixed

--- send_email.php
+++ send_email.php
@@ -1,5 +1,9 @@
 <?php
+$admin_email = "gordeev.dmitriy@physics.msu.ru";
+
 if(isset($_POST["email"]) && isset($_POST["message"])) {
-    mail($_POST["email"], "Какая-то тема письма", wordwrap($_POST["message"],70));
+
+    $_POST["message"] = $_POST["message"]."\nemail отправителя: ".$_POST["email"];
+    mail($admin_email, "Письмо с сайта-портфолио: gr-dev.ru", wordwrap($_POST["message"], 70));
     header("Location: index.html");
 }
